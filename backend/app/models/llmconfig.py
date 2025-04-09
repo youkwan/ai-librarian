1ad55a87a9cf3a9f73f64e1c3936595dfad5a279
youkwan <k123k123k23@gmail.com>
2025-04-09 09:28:53 +0800

    support numerous new models (Gemini 2.5, GPT4.5, Llama4, etc.) and remove deprecated model (Mistral 8x7b)

--- backend/app/models/llmconfig.py
+++ backend/app/models/llmconfig.py
@@ -12,26 +12,41 @@ class Model(str, Enum):
     since ReDoc does not respect Pydantic's default settings like Swagger and uses the first enum value as default.
     """
 
-    GPT_4O_MINI = "gpt-4o-mini"  # First model will be the default in ReDoc
-    GPT_4O = "gpt-4o"
-    O3_MINI = "o3-mini"
-    O1 = "o1"
-    O1_MINI = "o1-mini"
-    CLAUDE_3_7_SONNET = "claude-3-7-sonnet-latest"
-    CLAUDE_3_5_HAIKU = "claude-3-5-haiku-latest"
-    CLAUDE_3_5_SONNET = "claude-3-5-sonnet-latest"
+    # OpenAI
+    GPT_4O_MINI = "openai:gpt-4o-mini"  # First model will be the default in ReDoc
+    GPT_4O = "openai:gpt-4o"
+    GPT_4_5_PREVIEW = "openai:gpt-4.5-preview"
+    O3_MINI = "openai:o3-mini"
+    O1_MINI = "openai:o1-mini"
+    O1 = "openai:o1"
+
+    # Anthropic
+    CLAUDE_3_7_SONNET = "anthropic:claude-3-7-sonnet-latest"
+    CLAUDE_3_5_HAIKU = "anthropic:claude-3-5-haiku-latest"
+    CLAUDE_3_5_SONNET_V2 = "anthropic:claude-3-5-sonnet-latest"
+    CLAUDE_3_5_SONNET = "anthropic:claude-3-5-sonnet-20240620"
+
+    # Google GenAI
+    GEMINI_2_5_PRO_EXP_03_25 = "google_genai:gemini-2.5-pro-exp-03-25"
     GEMINI_2_0_FLASH = "google_genai:gemini-2.0-flash"
     GEMINI_2_0_FLASH_LITE = "google_genai:gemini-2.0-flash-lite"
     GEMINI_1_5_FLASH = "google_genai:gemini-1.5-flash"
     GEMINI_1_5_FLASH_8B = "google_genai:gemini-1.5-flash-8b"
     GEMINI_1_5_PRO = "google_genai:gemini-1.5-pro"
+
+    # Groq
+    LLAMA_4_SCOUT_17B_16E_INSTRUCT = "groq:meta-llama/llama-4-scout-17b-16e-instruct"
     LLAMA_3_3_70B_VERSATILE = "groq:llama-3.3-70b-versatile"
+    LLAMA_3_3_70B_SPECDEC = "groq:llama-3.3-70b-specdec"
     LLAMA_3_2_1B_PREVIEW = "groq:llama-3.2-1b-preview"
+    LLAMA_3_2_3B_PREVIEW = "groq:llama-3.2-3b-preview"
     LLAMA_3_1_8B_INSTANT = "groq:llama-3.1-8b-instant"
-    MIXTRAL_8X7B_32768 = "groq:mixtral-8x7b-32768"
+    MISTRAL_SABA_24B = "groq:mistral-saba-24b"
     QWEN_QWQ_32B = "groq:qwen-qwq-32b"
+    QWEN_2_5_CODER_32B = "groq:qwen-2.5-coder-32b"
     QWEN_2_5_32B = "groq:qwen-2.5-32b"
     DEEPSEEK_R1_DISTILL_QWEN_32B = "groq:deepseek-r1-distill-qwen-32b"
+    DEEPSEEK_R1_DISTILL_LLAMA_70B = "groq:deepseek-r1-distill-llama-70b"
 
     @classmethod
     def list(cls) -> list[str]:
