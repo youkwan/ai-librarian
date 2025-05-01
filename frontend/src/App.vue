3ed020f1c42d09b07ec70dc3ab826b6df172fa22
Zihao10 <zihaochen1028@gmail.com>
2025-05-01 16:12:47 +0800

    feat: chatAgent and live2D(vue.js)

---
+++ frontend/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import ChatAgent from './components/ChatAgent.vue';
+import live2D from './components/live2D.vue';
+</script>
+
+<template>
+<ChatAgent></ChatAgent>
+<live2D></live2D>
+</template>
+
+<style scoped>
+
+</style>
