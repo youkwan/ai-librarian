8dfd0faedf8de685610664c9e7f3a85d70d47618
youkwan <k123k123k23@gmail.com>
2025-07-29 23:41:46 +0800

    restructure project layout

---
+++ backend/ai_librarian_monorepo/ai_librarian_core/src/ai_librarian_core/tools/google_books.py
@@ -0,0 +1,33 @@
+from ai_librarian_core.utilities.google_books import RobustGoogleBooksAPIWrapper
+from langchain_core.callbacks import CallbackManagerForToolRun
+from langchain_core.tools import BaseTool
+from pydantic import BaseModel, Field
+
+
+class RobustGoogleBooksQueryInput(BaseModel):
+    """Input schema for the GoogleBooksQuery tool."""
+
+    query: str = Field(description="query to look up on google books")
+
+
+class RobustGoogleBooksQueryRun(BaseTool):
+    """Tool that searches the Google Books API."""
+
+    name: str = "GoogleBooks"
+    description: str = (
+        "A wrapper around Google Books. "
+        "Useful for when you need to answer general inquiries about "
+        "books of certain topics and generate recommendation based "
+        "off of key words"
+        "Input should be a query string"
+    )
+    api_wrapper: RobustGoogleBooksAPIWrapper = Field(default_factory=RobustGoogleBooksAPIWrapper)
+    args_schema: type[BaseModel] = RobustGoogleBooksQueryInput
+
+    def _run(
+        self,
+        query: str,
+        run_manager: CallbackManagerForToolRun | None = None,
+    ) -> str:
+        """Use the Google Books tool."""
+        return self.api_wrapper.run(query)
