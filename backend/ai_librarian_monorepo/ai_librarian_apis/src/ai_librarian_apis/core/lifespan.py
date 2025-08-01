8478ce7b65691996fac707bafd8ab658f9d0f0a7
youkwan <k123k123k23@gmail.com>
2025-08-01 17:42:47 +0800

    refactor lifespan function to integrate logging setup

--- backend/ai_librarian_monorepo/ai_librarian_apis/src/ai_librarian_apis/core/lifespan.py
+++ backend/ai_librarian_monorepo/ai_librarian_apis/src/ai_librarian_apis/core/lifespan.py
@@ -1,22 +1,10 @@
 from contextlib import asynccontextmanager
 
-from ai_librarian_apis.core.settings import Settings
+from ai_librarian_apis.core.logger import setup_logging
 from fastapi import FastAPI
-from rich import print
 
 
 @asynccontextmanager
-async def lifespan(the_app):
-    settings = Settings()
-    try:
-        yield
-    finally:
-        print("shutdown things")
-
-
-app = FastAPI(lifespan=lifespan)
-
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(app, port=5000, log_level="info", timeout_graceful_shutdown=3)
+async def lifespan(app: FastAPI):
+    setup_logging()
+    yield
