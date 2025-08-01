bfb9b59fbe907ee27f1a93d7edfa93cfacc314c0
youkwan <k123k123k23@gmail.com>
2025-08-01 17:53:26 +0800

    integrate lifespan into FastAPI app initialization

--- backend/ai_librarian_monorepo/ai_librarian_apis/src/ai_librarian_apis/main.py
+++ backend/ai_librarian_monorepo/ai_librarian_apis/src/ai_librarian_apis/main.py
@@ -6,23 +6,24 @@ from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
 from ai_librarian_apis.core.cors import setup_cors
+from ai_librarian_apis.core.lifespan import lifespan
 from ai_librarian_apis.core.settings import settings
 from ai_librarian_apis.routes.sys import sys_router
+from ai_librarian_apis.routes.tools import tools_router
 
 
 def create_app() -> FastAPI:
     load_dotenv()
     __version__ = metadata.version("ai-librarian-apis")
-    app = FastAPI(
-        title="AI Librarian APIs",
-        version=__version__,
-    )
+    app = FastAPI(title="AI Librarian APIs", version=__version__, lifespan=lifespan)
     setup_cors(app)
+
     app.include_router(sys_router, prefix="/v1")
+    app.include_router(tools_router, prefix="/v1")
     # app.include_router(agents_router, prefix="/v1")
 
     @app.get("/", include_in_schema=False)
-    async def redirect_root() -> RedirectResponse:  # type: ignore
+    async def redirect_root() -> RedirectResponse:
         return RedirectResponse(url="/redoc")
 
     return app
